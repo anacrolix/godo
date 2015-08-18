0d9ab1ae2f82df87a3572448959881c62b8be90b
Matt Joiner <anacrolix@gmail.com>
2015-08-18 17:52:24 +1000

    complete.sh: Fall back on the bash default autocomplete if we're not completing a package spec

--- complete.sh
+++ complete.sh
@@ -2,11 +2,16 @@ _godo() {
 	local cur prev
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[@]:1:COMP_CWORD-1}"
+    prev=("${COMP_WORDS[@]:1:COMP_CWORD-1}")
 
     if [[ ${cur} == -* ]]; then return; fi
     if [[ ${cur} == /* ]]; then return; fi
-    # if [[ ${cur} == .* ]]; then return; fi
+    for arg in "${prev[@]}"; do
+    	if [[ "${arg}" != -* ]]; then
+            # the package spec argument has already happened
+    		return
+    	fi
+    done
     COMPREPLY=( $(compgen -W "$(go-list-cmd ${cur})" -- ${cur}) )
 }
-complete -F _godo godo
+complete -F _godo -o default godo
